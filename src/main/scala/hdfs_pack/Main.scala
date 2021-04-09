62c11f98b5789a7ff3f721a9e8a5cbf1f8a6a54a
ozavgorodnyaya <ozavgorodnyaya@fil-it.ru>
2021-04-09 19:02:11 +0300

    refactoring

--- src/main/scala/hdfs_pack/Main.scala
+++ src/main/scala/hdfs_pack/Main.scala
@@ -9,53 +9,35 @@ object Main extends App {
 
   val fs = FileSystem.get(new URI("hdfs://localhost:9000"), new Configuration())
 
-  val srcPath = new Path("/stage")
-
   val dstPath = new Path("/ods")
   createFolder(dstPath)
 
   try {
-    fs.listStatus(srcPath).foreach(
-      x => {
-              val fileList = fs.listStatus(x.getPath)
+        fs.listStatus(new Path("/stage")).foreach(
+            x => {
+              println("moving directories " + x.getPath.getName + " to " + dstPath)
+              val ok = fs.rename(x.getPath, dstPath)
+                                        if (ok) {
+                                           println("moved " + x.getPath.getName)
+                                        } else {
+                                          println("Error! Not moved " + x.getPath.getName)
+                                        }
+            }
+          )
+          fs.listStatus(dstPath).foreach(
+                x => {
+                        val fileList = fs.listStatus(x.getPath)
                             .filter(y => y.getPath.getName.contains("csv"))
 
-              println("folder contains files .csv")
-                    fileList.foreach(x => println(x.getPath.getName))
+                        val resultFile = new Path(x.getPath, fileList(0).getPath.getName)
+                        val restFiles: Array[Path] = fileList.map(f => f.getPath).tail
 
-              val destDir = new Path(dstPath, x.getPath.getName)
-              createFolder(destDir)
-
-              println("moving .csv files to " + destDir)
-              fileList.foreach(fl => {
-                                        val ok = fs.rename(new Path(x.getPath, fl.getPath.getName), destDir)
-                                        if (ok) {
-                                           println("moved " + fl.getPath.getName)
-                                        }
-                                     }
-                              )
-
-              println("deleting " + x.getPath)
-              val ok = fs.delete(x.getPath, true)
-              if (ok) {
-                 println("deleted")
-              }
-      }
-    )
-
-    fs.listStatus(dstPath).foreach(
-      x => {
-              val fileList = fs.listStatus(x.getPath)
-              val resultFile = new Path(x.getPath, fileList(0).getPath.getName)
-              val restFiles: Array[Path] = fileList.map(fs => fs.getPath).tail
-
-              println("adding all files into " + resultFile)
-              if (restFiles.size > 0) {
-                fs.concat(resultFile, restFiles)
-              }
-      }
-    )
-
+                        println("adding all files into " + resultFile)
+                        if (restFiles.size > 0) {
+                          fs.concat(resultFile, restFiles)
+                        }
+                }
+            )
   } catch {
     case e: Exception => println(e.getMessage)
   } finally {
